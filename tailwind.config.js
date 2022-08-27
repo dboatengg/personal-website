3194b66e0bfb402d84f74c6c821b144efd89e19c
dboatengg <dicksonboateng67@gmail.com>
2022-08-27 14:25:36 +0000

    set up project

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  darkMode: "class", // 'false' or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      body: ["Epilogue", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      "background-dark": "#1e1e25",
+      "title-dark": "#ffffff",
+      "text-dark": "#a2a1a6",
+      "background-light": "#f5f6f5",
+      "card-light": "#ededed",
+      "title-light": "#373737",
+      "text-light": "#6a6a6a",
+      button: "#7364d0",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
